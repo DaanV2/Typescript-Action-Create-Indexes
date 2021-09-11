155bdfd1b58314c2a4170192675da4d9154a531b
DaanV2 <daanverstraten@hotmail.com>
2021-09-11 18:01:21 +0200

    Refactoring

--- lib/traverse.d.ts
+++
@@ -1,7 +0,0 @@
-import pm from "picomatch";
-/**
- * Creates a markdown index page
- * @param folder
- * @returns True if the page was made.
- */
-export declare function CreateFolder(folder: string, excludes: (pm.MatcherWithState | pm.Matcher)[]): boolean;
